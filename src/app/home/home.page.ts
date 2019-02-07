964f5aead60f1a47e938e53c3cb730f2c78f569b
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2019-02-07 05:42:13 -0500

    ✨ done

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  image: string;
+
+  constructor (
+    private camera: Camera,
+    private webView: WebView
+  ) {}
+
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.CAMERA
+    };
+    this.camera.getPicture(options)
+    .then((imageData) => {
+      this.image = this.webView.convertFileSrc(imageData);
+    }, (err) => {
+      console.log(err);
+    });
+  }
 
 }
